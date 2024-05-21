9a5ee534b3a1db4bed8b7f6bb341516a406de23f
Irina Bugrova <irina92-08@mail.ru>
2024-05-21 15:01:50 +0300

    modified: bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ import { program } from 'commander';
 program
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1> <filepath2>');
 
 program.parse();
