5b4cb3cd3404cddcc6334d9fa71dd7c683d25896
Irina Bugrova <irina92-08@mail.ru>
2024-06-17 19:46:53 +0300

    modified:   src/formatters/plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -10,33 +10,26 @@ const valueStr = (value) => {
 };
 
 const plain = (astData) => {
-  let updatedValue;
-
-  const iter = (data, path) => data.map((node) => {
+  const iter = (data, path) => data.reduce((acc, node, index, array) => {
     const pathResult = `${path}${node.key}`;
     const isNested = node.status === 'nested';
     if (isNested) {
-      return iter(node.value, `${pathResult}.`);
+      return [...acc, iter(node.value, `${pathResult}.`)];
     }
-
     switch (node.status) {
       case 'added':
-        return `Property '${pathResult}' was ${node.status} with value: ${valueStr(node.value)}`;
+        return [...acc, `Property '${pathResult}' was ${node.status} with value: ${valueStr(node.value)}`];
       case 'removed':
-        return `Property '${pathResult}' was ${node.status}`;
+        return [...acc, `Property '${pathResult}' was ${node.status}`];
       case 'updated':
         if (node.format === '-') {
-          updatedValue = node.value;
-          return '';
+          return acc;
         }
-        return `Property '${pathResult}' was updated. From ${valueStr(updatedValue)} to ${valueStr(node.value)}`;
+        return [...acc, `Property '${pathResult}' was updated. From ${valueStr(array[index - 1].value)} to ${valueStr(node.value)}`];
       default:
-        return '';
+        return acc;
     }
-  })
-    .filter(Boolean)
-    .join('\n');
-
+  }, []).join('\n');
   return iter(astData, '');
 };
 
